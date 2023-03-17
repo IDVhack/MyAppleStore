21e2adfe711f0a3316e4090a32997cf661476c60
Tyuerl <Gresso507@gmail.com>
2023-03-17 20:07:41 +0300

    Добавьте файлы проекта.

---
+++ MyAppleProject/Infrasrtucture/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using MyAppleProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MyAppleProject.Infrasrtucture
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+ 
+
+    }
+    public class DbContextOptions<T>
+    {
+    }
+}
---
+++ MyAppleProject/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyAppleProject.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, введите значение")]
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Минимальная длина составляет 2")]
+        public string Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Пожалуйста, введите значение")]
+        [Column(TypeName = "decimal(8,2")]
+        public decimal Price { get; set; }
+        public long CategoryId { get; set; }
+        public Category Category { get; set; }
+        public string Image { get; set; }
+
+    }
+}
